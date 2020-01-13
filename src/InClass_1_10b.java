6c41ab561262f891d53357db4c039804b961af71
david lash <dlash02@gmail.com>
2020-01-13 14:21:08 -0600

    Intial test push2 - Testing an update

--- src/InClass_1_10b.java
+++ src/InClass_1_10b.java
@@ -4,34 +4,41 @@ public class InClass_1_10b {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         Boolean keepGoing = true;
-        while( keepGoing ) {
+        while (keepGoing) {
             System.out.print("\n Enter a date MM/DD/YYYY format");
             String theString = s.next();
-            if( gotValidData( theString )){
-               System.out.print("Yes that a valid date") ;
+            if (gotValidData(theString)) {
+                System.out.print("Yes that a valid date");
             } else {
-                System.out.print("Nope! ... Not a valid date") ;
+                System.out.print("Nope! ... Not a valid date");
             }
         }
     }
+
     private static boolean gotValidData(String theString) {
         Boolean retVal = false;
+
+        String pattern = "\\d\\d/\\d\\d/\\d\\d\\d\\d";
+        if (!theString.matches(pattern)) {
+            System.out.printf("\n Error Wrong input pattern detected format:%s", theString);
+        }
         String toks[] = theString.split("/");
-        if ( toks.length != 3 ){
-            return false;
-        }
         String mm = toks[0];
-        if ( !gotValidMonth( mm )) {
+        if (toks.length != 3) {
             return false;
         }
-        return true;
-    }
-    private static boolean gotValidMonth(String mm) {
-        int nMon = Integer.parseInt( mm );
-        if ( nMon >= 1 && nMon <= 12 ) {
+
+            if (!gotValidMonth(mm)) {
+                return false;
+            }
             return true;
-        }  else {
-           return false;
+        }
+    private static boolean gotValidMonth(String mm) {
+        int nMon = Integer.parseInt(mm);
+        if (nMon >= 1 && nMon <= 12) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
---
+++ src/InClass_substring_1_13.java
@@ -0,0 +1,7 @@
+public class InClass_substring_1_13 {
+    public static void main(String[] args) {
+        String str = "AppleBanana" ;
+        System.out.printf("Substring=%s", str.substring(0,5));
+        System.out.printf("Substring2=%s", str.substring(5));
+    }
+}
